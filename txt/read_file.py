94d2e65bf3c790c696ce38f9b1be08f2bc811148
Samuel Hurtado <samuelhurtado20@hotmail.com>
2024-09-14 22:19:08 -0500

    Update read_file.py

--- txt/read_file.py
+++ txt/read_file.py
@@ -15,3 +15,15 @@
 #Sobreescribir el texto
 with open('caperucita.txt', 'w') as file:
     file.write("\n\nBy:ChatGPT")
+
+# RETO conteo de las lineas del cuento de caperucita
+with open ("clase30/caperucita.txt", "r") as file:
+    lines = file.readlines()
+    print(len(lines)) # 63
+
+with open('cuento.txt','r') as file:
+    number_lines=0
+    for lines in file:
+        number_lines+=1
+        print(f"{number_lines}.{lines.strip()}")
+```with open('cuento.txt','r') as file:    number\_lines=0    for lines in file:        number\_lines+=1        print(f"{number\_lines}.{lines.strip()}")
